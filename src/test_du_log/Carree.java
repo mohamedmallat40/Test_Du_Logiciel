2e0088daf9732c5f0e27f9eff2c7bc348c83c643
mohamed mallat <mohamedmallat40@users.noreply.github.com>
2019-04-17 02:25:39 +0100

    creation d'une petite application pour la tester avec le junit

---
+++ src/test_du_log/Carree.java
@@ -0,0 +1,18 @@
+
+package test_du_log;
+
+/**
+ *
+ * @author mohammed mallat et nawres mahfoudhi 2LFIG G03
+ * 
+ */
+public class Carree {
+    private double sidelen;
+    public Carree(double len){
+        sidelen = len;}
+    public double zone(){
+        return sidelen*sidelen;}
+    public void setsidelength(){
+        
+        }
+}
